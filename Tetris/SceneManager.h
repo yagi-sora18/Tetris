f5ff0df305822818d899853277c9fc73d4ef791b
yagi-sora18 <138739113+yagi-sora18@users.noreply.github.com>
2023-10-03 15:00:07 +0900

    ヘッダーを追加
    ヘッダーを追加しました。

--- Tetris/SceneManager.h
+++ Tetris/SceneManager.h
@@ -1,1 +1,41 @@
 #pragma once
+
+/**********************************
+*マクロ定義
+***********************************/
+#define D_ERROR     (-1)
+#define D_NORMALITY (0)
+
+/**********************************
+*型定義
+***********************************/
+enum GAME_MODE
+{
+	E_TITLE,
+	E_GAMEMAIN,
+	E_RANKING,
+	E_END,
+	E_MODE_MAX
+};
+
+/**********************************
+*プロトタイプ宣言
+***********************************/
+void SceneManager_Initialize(GAME_MODE mode);    //初期化処理
+void SceneManager_Update(void);                              //更新処理
+void SceneManager_Draw(void);                                //描画処理
+
+void Change_Scene(GAME_MODE mode);                           //シーン変更処理
+int ErrorCheck(void);                                        //エラーチェック処理
+
+
+
+
+
+
+
+
+
+
+
+
